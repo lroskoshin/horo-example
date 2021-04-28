2436463dee51fbb6930e45d74c9027fd85387d21
lroskoshin <lev_roskoshin@protonmail.com>
2021-04-28 15:43:53 +0300

    added todo sketch

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import { horo } from 'horojs';
 import { Counter } from './components/counter';
+import { ToDo } from './components/todo';
 
 
 export const app = horo`
     <section>
         <h2>Counter</h2>
         ${Counter()}
+    </section>
+    <section>
+        <h2>ToDo</h2>
+        ${ToDo()}
     </section>
 `;
---
+++ src/components/todo.ts
@@ -0,0 +1,20 @@
+import { horo } from 'horojs';
+import { animationFrameScheduler, from, scheduled } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { map, switchMap } from 'rxjs/operators';
+
+export function ToDo() {
+    const todo$ = fromFetch('https://jsonplaceholder.typicode.com/todos/1').pipe(
+        switchMap((response: Response) => {
+            return from(response.json());
+        }),
+        map((json: any) => {
+            return json.title;    
+        })
+    );
+    return scheduled([horo`
+        <ul>
+            <li>${todo$}</li>
+        </ul>
+    `], animationFrameScheduler);
+}
