5e74a521fd4883b800a3795789db9a2d4dcb7031
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-26 13:34:58 +0300

    migrate to new horo

--- src/components/todo.ts
+++ src/components/todo.ts
@@ -1,5 +1,5 @@
 import { horo, Component } from 'horojs';
-import { from, Observable } from 'rxjs';
+import { from } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 import { map, switchMap } from 'rxjs/operators';
 
@@ -10,27 +10,24 @@ type Todo = {
     completed: false,
 }
 
-export function ToDoComponent(): Observable<Component> {
-    return fromFetch('https://jsonplaceholder.typicode.com/todos').pipe(
+export function ToDoComponent(): Component {
+    const list = fromFetch('https://jsonplaceholder.typicode.com/todos').pipe(
         switchMap((response: Response) => {
             return from(response.json());
         }),
         map((todos : Todo[]) => {
             const components = todos.map(TodoItem);
-            return mergeDocumentComponents(components);
+            return components;
         })
     );
+
+    return horo`
+        <ul>
+            ${list}
+        </ul>
+    `;
 }
 
 function TodoItem(todo: Todo) {
     return horo`<li><b>#${String(todo.id)}<b> ${todo.title} ${todo.completed ? '+' : '-'}</li>`;
-}
-
-function mergeDocumentComponents(components: Component[]): Component {
-    const root = document.createDocumentFragment();
-    root.append(...components.map((component: Component) => component.fragment));
-    return {
-        fragment: root,
-        delete: () => components.forEach((component: Component) => component.delete())
-    };
 }
